1798c1cc85b4ebef787fe7aabf166be7d81ffb7f
Stanley Law <hfcs@users.noreply.github.com>
2017-02-25 15:37:08 +0800

    knock detection and timer state machine

--- nodemcu_target.ino
+++ nodemcu_target.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "Arduino.h"
 
+#define ADC_PIN A0
+
+
+const int KNOCK_ADC_THRESHOLD = 500;
+enum TIMER_STATE { // Timer state machine reflects stop plate for simplicity
+  TIMER_RESET,
+  TIMER_START,
+  TIMER_TIME_START,
+  TIMER_LED_1,
+  TIMER_LED_2
+} timer_state;
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+  pinMode(ADC_PIN, INPUT);
+}
+
+bool pollKnock() {
+  if ((unsigned int) analogRead(ADC_PIN) >= KNOCK_ADC_THRESHOLD) {
+    //Serial.println("Knock");
+    return true;
+  }
+  return false;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  pollKnock();
+  delay(1000);
 }
