6f276bfadde8dc803a885a0d252358d63fa46a72
Stanley Law <hfcs@users.noreply.github.com>
2017-03-13 01:28:15 +0800

    fixed a rounding issue time print out

--- utils.cpp
+++ utils.cpp
@@ -14,5 +14,8 @@ String millisToTimeString(unsigned long elapse) {
   unsigned long minute = elapse / 1000 / 60;
   unsigned long second = (elapse % (1000 * 60)) / 1000;
   unsigned long deciSecond = (elapse % 1000) / 10;
+  if ((elapse % 10) > 4) { // round to the nearest deciSecond
+    deciSecond++;
+  }
   return digitToString(minute) + ":" + digitToString(second) + ":" + digitToString(deciSecond);
 }
