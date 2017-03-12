8bb54cdc34e916fa175b80f0719ca4672c097bb6
Stanley Law <hfcs@users.noreply.github.com>
2017-03-12 19:04:19 +0800

    implement record hit

--- stopwatch.cpp
+++ stopwatch.cpp
@@ -7,8 +7,10 @@
 // All timestamps are in millis as it overflows in 50 days, more than enough
 // for running a shooting match
 
+#define MAX_HITS (4 * 28) // for practical purpose, someone is not gonna dump 4 magazines of open diviion into the stop plate
+
 unsigned long cofStartMillis; // millis for course fire start, AKA starting beep
-unsigned long cofStopplateMillis[4 * 28]; // for practical purpose, someone is not gonna dump 4 magazines of open diviion into the stop plate
+unsigned long cofStopplateMillis[MAX_HITS];
 unsigned long currentShotIndex;
 
 void updateStopwatchDisplay() {
@@ -16,11 +18,19 @@ void updateStopwatchDisplay() {
   writeSecondRow(millisToTimeString(cofElapseMillis));
 }
 
-
-void setCofStartMillis(int event, int param) {
+void startStopwatch(int event, int param) {
+  currentShotIndex = 0;
   cofStartMillis = millis();
 }
 
+void recordHit(int event, int param) {
+  if (currentShotIndex < MAX_HITS) {
+    cofStopplateMillis[currentShotIndex++] = millis();
+  }
+  // TODO: update screen to latest hit
+}
+
 void setupStopwatch() {
-  TimerEvent::getInstance()->addListener(TimerEvent::hardwareStopwatchStart, setCofStartMillis);
+  TimerEvent::getInstance()->addListener(TimerEvent::hardwareStopwatchStart, startStopwatch);
+  TimerEvent::getInstance()->addListener(TimerEvent::hardwareStopwatchRecordHit, recordHit);
 }
