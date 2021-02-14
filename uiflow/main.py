12db9b69b21a8c583d40c1597b2dced010fb26d8
Stanley Law <hfcs@users.noreply.github.com>
2021-02-14 15:45:29 +0800

    Align recorded shot time and display time

--- uiflow/main.py
+++ uiflow/main.py
@@ -8,8 +8,9 @@ setScreenColor(0xcccccc)
 
 timeMilli = None
 countdownMilli = None
+shotTimeMilli = None
 startSignalTicks = None
-shotsMilli = None
+shotTimeListMilli = None
 
 
 title = M5Title(title="Shot Count Down Timer", x=3, fgcolor=0xFFFFFF, bgcolor=0xff0035)
@@ -24,17 +25,17 @@ import math
 
 # Refresh screen with timer reading, parameter in milliseconds
 def refreshTimer(timeMilli):
-  global countdownMilli, startSignalTicks, shotsMilli
+  global countdownMilli, shotTimeMilli, startSignalTicks, shotTimeListMilli
   labelTimer.setText(str("%.2f"%((timeMilli / 1000))))
 
 # Print the list of recorded shots. Input: the global list
 def refreshShots():
-  global timeMilli, countdownMilli, startSignalTicks, shotsMilli
-  labelShots.setText(str((str('shots:') + str(str(shotsMilli)))))
+  global timeMilli, countdownMilli, shotTimeMilli, startSignalTicks, shotTimeListMilli
+  labelShots.setText(str((str('shots:') + str(str(shotTimeListMilli)))))
 
 # Timer reset logic
 def resetCountDown():
-  global timeMilli, countdownMilli, startSignalTicks, shotsMilli
+  global timeMilli, countdownMilli, shotTimeMilli, startSignalTicks, shotTimeListMilli
   startSignalTicks = 0
   timerSch.stop('timerCountDown')
   timerSch.stop('timeRefreshTick')
@@ -42,47 +43,52 @@ def resetCountDown():
 
 # Timer start logic
 def startCountDown(countdownMilli):
-  global timeMilli, startSignalTicks, shotsMilli
+  global timeMilli, shotTimeMilli, startSignalTicks, shotTimeListMilli
   if startSignalTicks == 0:
     startSignalTicks = (time.ticks_ms()) + countdownMilli
     timerSch.run('timerCountDown', countdownMilli, 0x01)
     timerSch.run('timeRefreshTick', 57, 0x00)
-    shotsMilli = []
+    shotTimeListMilli = []
     refreshShots()
+
+# Handle the shot including timer update, and previous shot time recording
+def recordShot(shotTimeMilli):
+  global timeMilli, countdownMilli, startSignalTicks, shotTimeListMilli
+  if len(shotTimeListMilli) >= 20:
+    shotTimeListMilli.pop()
+  shotTimeListMilli.insert(0, "%.2f"%((shotTimeMilli / 1000)))
+  refreshTimer(shotTimeMilli)
+  refreshShots()
 
 
 def buttonA_wasPressed():
-  global startSignalTicks, timeMilli, shotsMilli, countdownMilli
+  global startSignalTicks, shotTimeListMilli, timeMilli, countdownMilli, shotTimeMilli
   resetCountDown()
   pass
 btnA.wasPressed(buttonA_wasPressed)
 
 def buttonC_wasPressed():
-  global startSignalTicks, timeMilli, shotsMilli, countdownMilli
+  global startSignalTicks, shotTimeListMilli, timeMilli, countdownMilli, shotTimeMilli
   startCountDown(random.randint(1000, 4000))
   pass
 btnC.wasPressed(buttonC_wasPressed)
 
 def buttonB_wasPressed():
-  global startSignalTicks, timeMilli, shotsMilli, countdownMilli
+  global startSignalTicks, shotTimeListMilli, timeMilli, countdownMilli, shotTimeMilli
   timerSch.stop('timeRefreshTick')
-  refreshTimer(math.fabs(startSignalTicks - (time.ticks_ms())))
-  if len(shotsMilli) >= 20:
-    shotsMilli.pop()
-  shotsMilli.insert(0, "%.2f"%((((time.ticks_ms()) - startSignalTicks) / 1000)))
-  refreshShots()
+  recordShot((time.ticks_ms()) - startSignalTicks)
   pass
 btnB.wasPressed(buttonB_wasPressed)
 
 @timerSch.event('timeRefreshTick')
 def ttimeRefreshTick():
-  global startSignalTicks, timeMilli, shotsMilli, countdownMilli
+  global startSignalTicks, shotTimeListMilli, timeMilli, countdownMilli, shotTimeMilli
   refreshTimer(math.fabs(startSignalTicks - (time.ticks_ms())))
   pass
 
 @timerSch.event('timerCountDown')
 def ttimerCountDown():
-  global startSignalTicks, timeMilli, shotsMilli, countdownMilli
+  global startSignalTicks, shotTimeListMilli, timeMilli, countdownMilli, shotTimeMilli
   timerSch.stop('timerCountDown')
   timerSch.stop('timeRefreshTick')
   speaker.sing(889, 1)
